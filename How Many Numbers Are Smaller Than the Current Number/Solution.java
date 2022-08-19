03b68c73416c7e3c11ff23034b39b7cb402f60dc
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2022-08-19 22:04:05 +0300

    ninth problem on the spread sheet

--- How Many Numbers Are Smaller Than the Current Number/Solution.java
+++ How Many Numbers Are Smaller Than the Current Number/Solution.java
@@ -1,15 +1,29 @@
 class Solution {
-    public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] countArray = new int[nums.length];
-        for(int i=0;i< countArray.length;i++){
-            countArray[i]=0;
-            for(int j=0;j< nums.length;j++){
-                if(nums[j] < nums[i]){
-                    countArray[i]++;
+    public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> result = new ArrayList<>();
+        // Sorted list using selection sort
+        int min;
+        int temp;
+        for(int k=0;k< nums.length-1;k++){
+            min= k;
+            for(int j=k+1;j< nums.length;j++){
+                if(nums[j]<nums[min]){
+                    min= j;
                 }
             }
+            if(min != k){
+//                Swap
+                temp=nums[k];
+                nums[k]=nums[min];
+                nums[min]=temp;
+            }
         }
-
-        return countArray;
+        // set index in result where target appeared
+        for(int i=0;i< nums.length;i++){
+            if(nums[i]== target){
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
