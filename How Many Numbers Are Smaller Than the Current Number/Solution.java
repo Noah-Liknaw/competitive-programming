98e6d670faa67abbc261c4386dbfc53e192c2a75
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2022-08-19 22:19:02 +0300

    updated wrong commit

--- How Many Numbers Are Smaller Than the Current Number/Solution.java
+++ How Many Numbers Are Smaller Than the Current Number/Solution.java
@@ -1,29 +1,15 @@
 class Solution {
-    public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer> result = new ArrayList<>();
-        // Sorted list using selection sort
-        int min;
-        int temp;
-        for(int k=0;k< nums.length-1;k++){
-            min= k;
-            for(int j=k+1;j< nums.length;j++){
-                if(nums[j]<nums[min]){
-                    min= j;
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] countArray = new int[nums.length];
+        for(int i=0;i< countArray.length;i++){
+            countArray[i]=0;
+            for(int j=0;j< nums.length;j++){
+                if(nums[j] < nums[i]){
+                    countArray[i]++;
                 }
             }
-            if(min != k){
-//                Swap
-                temp=nums[k];
-                nums[k]=nums[min];
-                nums[min]=temp;
-            }
         }
-        // set index in result where target appeared
-        for(int i=0;i< nums.length;i++){
-            if(nums[i]== target){
-                result.add(i);
-            }
-        }
-        return result;
+
+        return countArray;
     }
 }
