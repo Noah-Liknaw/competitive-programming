ccd0eb3f454e953be2971705b7a9d2a63a48e676
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2022-08-28 18:54:29 +0300

    Two pointer problem 1

---
+++ Move Zeros/Solutiont.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        using two pointers
+        int i=0,j=0,temp=0;
+        if(nums.length != 1){
+        while(jnums.length && inums.length){
+            if(nums[i] != nums[j]){
+                swap
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            if(nums[i] != 0 ){
+                i++;
+            }
+            j++;
+        }
+        }
+    }
+}
